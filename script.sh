37ddcb10d2da4fe708dbcd9409e8970c745e29f6
veram96 <angelvemo@gmail.com>
2023-03-26 18:35:49 -0600

    script correcton

--- script.sh
+++ script.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-echo "Es Viernes por la tarde :D"
-echo -e "Estas por iniciar el deployment de una aplicación\n"
-sleep 10
-if [ "$PROD_ENV" = "true" ]
+echo "Esta es la actividad 1"
+echo "Para está actividad pondré a prueba lo que aprendí en la clase $NO_CLASE"
+echo -e "\nPrimero quiero presentarme: Me llamo $MI_NOMBRE"
+
+if [ "$EMPRESA" = "Liverpool" ]
 then
-  echo "Ups! No realizaste pruebas de Integración y tiraste Prod en Viernes :("
+    echo "Trabajo en una empresa muy chida que se llama $EMPRESA y me da este curso grati."
 else
-  echo "Tiraste el ambiente, pero no es prod. El lunes sin falta krnal"
+    echo "Trabajo en una empresa no muy chida que se llama $EMPRESA y pagué por este curso."
 fi
+
+if [ "$ASISTENCIA" = "false" ]
+then
+  echo -e "\nEn mi caso no asistí a la clase $NO_CLASE, por lo que no aprendí nada"
+else
+  echo -e "\nEn las siguientes tres líneas apareceran 3 cosas que aprendí:\n$COSAS_QUE_APRENDI"
+fi
+
+echo -e "\nGracias por su atención."
